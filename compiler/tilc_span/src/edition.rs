0015a12db5d77b00f989067d1f9290013a626b1a
Alibek <freed.alibek@gmail.com>
2024-10-13 19:27:32 +0800

    Edition enum for parsing session struct

---
+++ compiler/tilc_span/src/edition.rs
@@ -0,0 +1,7 @@
+pub enum Edition {
+  // Don't know the release year yet
+  E20xx,
+
+  // I'll try to create features every 3-5 years
+  E20xxPlus3or5,
+}
--- compiler/tilc_span/src/lib.rs
+++ compiler/tilc_span/src/lib.rs
@@ -1,8 +1,10 @@
+mod edition;
 mod pos;
 mod source_file;
 mod span;
 
 
+pub use edition::*;
 pub use pos::*;
 pub use source_file::*;
 pub use span::*;
