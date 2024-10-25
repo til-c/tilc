c1935af0a0d9363b2f498277232c91475f670e96
Alibek <freed.alibek@gmail.com>
2024-10-25 19:51:02 +0800

    unidirectional vector

--- compiler/tilc_data_structures/src/lib.rs
+++ compiler/tilc_data_structures/src/lib.rs
@@ -1,1 +1,20 @@
+use std::ops::Deref;
 
+pub struct UnidirectionalVector<T>(Vec<T>);
+impl<T> UnidirectionalVector<T> {
+  pub fn push(&mut self, item: T) {
+    self.0.push(item);
+  }
+}
+impl<T> Default for UnidirectionalVector<T> {
+  fn default() -> Self {
+    return Self(Vec::<T>::new());
+  }
+}
+impl<T> Deref for UnidirectionalVector<T> {
+  type Target = Vec<T>;
+
+  fn deref(&self) -> &Self::Target {
+    return &self.0;
+  }
+}
