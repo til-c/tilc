424d8e032796bfe13c6f2e1f11fe6fda0cd51147
Alibek <freed.alibek@gmail.com>
2024-11-06 12:09:38 +0800

    parse session struct

--- compiler/tilc_session/src/parse.rs
+++ compiler/tilc_session/src/parse.rs
@@ -1,10 +1,24 @@
-use std::collections::HashMap;
+use std::sync::RwLock;
 
+use indexmap::IndexMap;
 use tilc_span::{Edition, Span, Symbol};
 
 
 pub struct SymbolRepo {
-  pub symbols: HashMap<Symbol, Span>,
+  pub symbols: RwLock<IndexMap<Symbol, Span>>,
+}
+impl SymbolRepo {
+  pub fn new() -> Self {
+    return Self {
+      symbols: RwLock::new(IndexMap::new()),
+    };
+  }
+
+
+  pub fn insert(&self, symbol: Symbol, span: Span) {
+    debug_assert!(!self.symbols.is_poisoned());
+    self.symbols.write().unwrap().entry(symbol).or_insert(span);
+  }
 }
 pub struct ParseSession {
   pub edition: Edition,
