ebfabe8a79f0bac9eab4cce74e1125317ea8a251
Alibek <freed.alibek@gmail.com>
2024-11-06 11:57:37 +0800

    intern method and derive attributes

--- compiler/tilc_span/src/symbol.rs
+++ compiler/tilc_span/src/symbol.rs
@@ -1,16 +1,25 @@
 use tilc_index::uidx;
 
-uidx!(SymbolIdx; Debug, PartialEq);
+use crate::{with_session_globals, SessionGlobals};
+
+uidx!(SymbolIdx; Clone, Copy, Debug, PartialEq, Eq, Hash);
 
 
-#[derive(Debug, PartialEq)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 pub struct Symbol(SymbolIdx);
 impl Symbol {
-  pub fn new(idx: u32) -> Self {
+  pub const fn new(idx: u32) -> Self {
     return Self(SymbolIdx(idx));
   }
 
   pub fn idx(&self) -> usize {
     return self.0.idx();
+  }
+
+
+  pub fn intern(string: &str) -> Self {
+    return with_session_globals(|session_globals: &SessionGlobals| {
+      return session_globals.symbol_interner.intern(string);
+    });
   }
 }
