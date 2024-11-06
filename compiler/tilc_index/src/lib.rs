eeb4ef85331b3b692b13ec775b4d07cad22f0de5
Alibek <freed.alibek@gmail.com>
2024-11-06 11:55:22 +0800

    new method for index struct

--- compiler/tilc_index/src/lib.rs
+++ compiler/tilc_index/src/lib.rs
@@ -1,10 +1,14 @@
 #[macro_export]
 macro_rules! uidx {
-  ($name:ident; $($derives:ident),*) => {
+  ($name:ident$(;),* $($derives:ident),*) => {
     #[derive($($derives),*)]
     pub struct $name(u32);
 
     impl $name {
+      pub const fn new(idx: u32) -> Self {
+        return Self(idx);
+      }
+
       pub fn idx(&self) -> usize {
         return self.0 as usize;
       }
