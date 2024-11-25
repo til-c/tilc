4905d9f60f2f31c72fd2fd0668415c59787d4832
Alibek <freed.alibek@gmail.com>
2024-11-25 10:50:34 +0800

    main file run tweaks

--- compiler/tilc/src/main.rs
+++ compiler/tilc/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-  println!("hi");
+use tilc_interface::{catch_if_error, Compiler};
+
+
+fn main() -> ! {
+  // let args = {
+  //   let mut vec: Vec<String> = Vec::new();
+  //   for (i, arg) in std::env::args().enumerate() {
+  //     vec.push(arg);
+  //   }
+  //   vec
+  // };
+  let args: Vec<String> =
+    std::env::args().enumerate().map(|(_, arg)| arg).collect();
+
+
+  let exit_code: i32 = match catch_if_error(|| {
+    return Compiler::new(&args).run();
+  }) {
+    Ok(_) => 0,
+    Err(_) => 1,
+  };
+  std::process::exit(exit_code)
 }
