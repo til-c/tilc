bba0008e16634070262b21469a7fe9288d57afc5
Alibek Bekzhanov <freed.alibek@gmail.com>
2025-07-02 17:58:12 +0800

    base for simple compiler run

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-  println!("Hello, world!");
+use tilc_interface::catch_if_error;
+
+
+fn main() -> ! {
+  let exit_code = match catch_if_error(|| {
+    // TODO: Run compiler here
+  }) {
+    Ok(_) => 0,
+    Err(_) => 1,
+  };
+  std::process::exit(exit_code);
 }
