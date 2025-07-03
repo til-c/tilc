08cd2fbb2c2d65b9cc16c3ebbddc70c013710a29
Alibek Bekzhanov <freed.alibek@gmail.com>
2025-07-03 18:55:35 +0800

    parse session struct (future add diagnostic handler to it)

--- compiler/tilc_session/src/parse.rs
+++ compiler/tilc_session/src/parse.rs
@@ -1,3 +1,5 @@
+use std::rc::Rc;
+
 use tilc_span::{Edition, SourceMap};
 
 
@@ -5,5 +7,14 @@ use tilc_span::{Edition, SourceMap};
 pub struct ParseSession {
   pub edition: Edition,
 
-  source_map: SourceMap,
+  source_map: Rc<SourceMap>,
+}
+impl ParseSession {
+  pub fn new(source_map: Rc<SourceMap>) -> Self {
+    return Self {
+      edition: Edition::default(),
+
+      source_map,
+    };
+  }
 }
