db4cdb44f0af6b0d6f40e3c6b9f45347cee95198
Alibek Bekzhanov <freed.alibek@gmail.com>
2025-07-03 18:02:17 +0800

    file name definitions

--- compiler/tilc_span/src/lib.rs
+++ compiler/tilc_span/src/lib.rs
@@ -1,7 +1,25 @@
+use std::path::PathBuf;
+
 #[derive(Debug, Clone, Copy)]
 pub struct ErrorGuaranteed(());
 impl ErrorGuaranteed {
   pub fn new_unchecked() -> Self {
     return Self(());
   }
+}
+
+
+#[derive(Debug)]
+pub enum RealFileName {
+  Local(PathBuf),
+  Remapped {
+    local_path: Option<PathBuf>,
+    virtual_path: PathBuf,
+  },
+}
+#[derive(Debug)]
+pub enum FileName {
+  Real(RealFileName),
+  // TODO: make Hash struct for simple u64 hashing
+  Anon(),
 }
