6a990707232dad3cab8748b5c04a9242e9dbe4d2
Alibek Bekzhanov <freed.alibek@gmail.com>
2025-07-04 16:49:42 +0800

    adding symbol repo attribute to parse session

--- compiler/tilc_session/src/parse.rs
+++ compiler/tilc_session/src/parse.rs
@@ -1,11 +1,14 @@
 use std::rc::Rc;
 
-use tilc_span::{Edition, SourceMap};
+use indexmap::IndexMap;
+use parking_lot::RwLock;
+use tilc_span::{Edition, SourceMap, Span, Symbol};
 
 
 #[derive(Debug)]
 pub struct ParseSession {
   pub edition: Edition,
+  pub symbol_repo: SymbolRepo,
 
   source_map: Rc<SourceMap>,
 }
@@ -13,8 +16,20 @@ impl ParseSession {
   pub fn new(source_map: Rc<SourceMap>) -> Self {
     return Self {
       edition: Edition::default(),
+      symbol_repo: SymbolRepo::default(),
 
       source_map,
     };
+  }
+}
+
+#[derive(Debug, Default)]
+pub struct SymbolRepo {
+  pub symbols: RwLock<IndexMap<Symbol, Span>>,
+}
+impl SymbolRepo {
+  pub fn insert(&self, symbol: Symbol, span: Span) {
+    debug_assert!(!self.symbols.is_locked());
+    self.symbols.write().entry(symbol).or_insert_with(|| span);
   }
 }
