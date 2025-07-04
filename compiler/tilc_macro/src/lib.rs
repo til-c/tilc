689d5ff1cf91fbbfd25d32363d0547a112dcf91a
Alibek Bekzhanov <freed.alibek@gmail.com>
2025-07-04 08:55:17 +0800

    symbols proc macro

--- compiler/tilc_macro/src/lib.rs
+++ compiler/tilc_macro/src/lib.rs
@@ -1,3 +1,4 @@
+mod symbols;
 mod uidx;
 
 use proc_macro::TokenStream;
@@ -6,4 +7,9 @@ use proc_macro::TokenStream;
 #[proc_macro]
 pub fn uidx(input: TokenStream) -> TokenStream {
   return uidx::uidx(input);
+}
+
+#[proc_macro]
+pub fn symbols(input: TokenStream) -> TokenStream {
+  return symbols::symbols(input);
 }
