8be0e46631c7d8754a18fc1551a9b74e4b91bd6f
Alibek Bekzhanov <freed.alibek@gmail.com>
2025-07-08 19:40:13 +0800

    ast items

--- compiler/tilc_ast/src/sandyq.rs
+++ compiler/tilc_ast/src/sandyq.rs
@@ -1,5 +1,7 @@
 use tilc_macro::uidx;
-use tilc_span::{Ident, Span};
+use tilc_span::{Ident, Span, Symbol};
+
+use crate::{DelimSpacing, DelimSpan, TokenStream};
 
 
 uidx! {
@@ -8,45 +10,203 @@ uidx! {
 }
 pub const SANDYQ_NODE_IDX: NodeIdx = NodeIdx::from_u32(0);
 
-#[derive(Debug)]
-pub struct Item<K = ItemKind> {
-  pub idx: NodeIdx,
-
-  pub ident: Ident,
-  pub span: Span,
-  pub vis: Visibility,
-
-  pub kind: K,
+uidx! {
+  #[derive(Debug, Clone, Copy)]
+  pub struct AttrIdx {}
 }
 
-#[derive(Debug)]
-pub enum ItemKind {
-  Fn(Box<Fn>),
-  Use(Box<Use>),
-}
+
 #[derive(Debug)]
 pub struct Sandyq {
   pub idx: NodeIdx,
 
+  pub attrs: Vec<Attribute>,
   pub items: Vec<Item>,
 
   pub span: Span,
 }
 
-
 #[derive(Debug)]
-pub struct Visibility {
-  pub kind: VisibilityKind,
+pub struct Attribute {
+  pub idx: AttrIdx,
+
+  pub path: Path,
+  pub args: AttrArgs,
+
+  pub style: AttrkibuteStyle,
   pub span: Span,
 }
 #[derive(Debug)]
-pub enum VisibilityKind {
-  Public,
-  Private,
-  Protected,
+pub enum AttrkibuteStyle {
+  Inner,
+  Outer,
+}
+#[derive(Debug)]
+pub enum AttrArgs {
+  Empty,
+  Delimited(DelimArgs),
+  Eq {
+    eq_span: Span,
+    expr: Box<Expression>,
+  },
+}
+
+#[derive(Debug)]
+pub struct Item<K = ItemKind> {
+  pub idx: NodeIdx,
+
+  pub attrs: Vec<Attribute>,
+  pub vis: Visibility,
+  pub kind: K,
+  pub ident: Ident,
+
+  pub span: Span,
+}
+
+#[derive(Debug)]
+pub enum ItemKind {
+  Static(Box<Static>),
+  Const(Box<Const>),
+  Use(Box<Use>),
+  Fn(Box<Fn>),
+  Korpe(Safety, Ident, Korpe),
+  ForeignKorpe(ForeignKorpe),
+  TyAlias(Box<TyAlias>),
+  Enum(Ident, Generics, Vec<EnumVariant>),
+  Struct(Ident, Generics, VariantKind),
+  Union(Ident, Generics, VariantKind),
+  Trait(Box<Trait>),
+  Impl(Box<Impl>),
+  MacroCall(Box<MacroCall>),
+  MacroDef(Ident, MacroDef),
+}
+#[derive(Debug)]
+pub enum ForeignItemKind {
+  Static(Box<Static>),
+  Fn(Box<Fn>),
+}
+#[derive(Debug)]
+pub enum AssociatedItemKind {
+  Const(Box<Const>),
+  Fn(Box<Fn>),
+  TyAlias(Box<TyAlias>),
+}
+
+#[derive(Debug)]
+pub struct MacroDef {
+  pub body: DelimArgs,
+  pub is_macro_rules: bool,
+}
+#[derive(Debug)]
+pub struct MacroCall {
+  pub ident: Ident,
+  pub args: DelimArgs,
+}
+#[derive(Debug)]
+pub struct Impl {
+  pub safety: Safety,
+  pub generics: Generics,
+  pub kind: ImplKind,
+  pub self_ty: Box<Ty>,
+  pub items: Vec<Item<AssociatedItemKind>>,
+
+  pub defaultness: Defaultness,
+  pub span: Span,
+}
+#[derive(Debug)]
+pub enum ImplKind {
+  Positive,
+  Negative(Span),
+}
+
+#[derive(Debug)]
+pub struct Trait {
+  pub idx: NodeIdx,
+
+  pub vis: Visibility,
+  pub safety: Safety,
+  pub ident: Ident,
+  pub items: Vec<Item<AssociatedItemKind>>,
+
+  pub span: Span,
+}
+
+#[derive(Debug)]
+pub struct EnumVariant {
+  pub idx: NodeIdx,
+
+  pub attrs: Vec<Attribute>,
+  pub ident: Ident,
+
+  pub span: Span,
+}
+#[derive(Debug)]
+pub enum VariantKind {
+  Struct(Vec<FieldDef>),
+  Tuple(Vec<FieldDef>),
+  Unit,
+}
+
+#[derive(Debug)]
+pub struct FieldDef {
+  pub idx: NodeIdx,
+
+  pub attrs: Vec<Attribute>,
+  pub vis: Visibility,
+  pub ident: Option<Ident>,
+  pub ty: Box<Ty>,
+
+  pub span: Span,
+}
+#[derive(Debug)]
+pub struct TyAlias {
+  pub vis: Visibility,
+  pub ident: Ident,
+  pub generics: Generics,
+  pub ty: Option<Box<Ty>>,
+
+  pub defaultness: Defaultness,
+}
+#[derive(Debug)]
+pub struct Static {
+  pub idx: NodeIdx,
+
+  pub attrs: Vec<Attribute>,
+  pub vis: Visibility,
+  pub mutability: Mutability,
+  pub ident: Ident,
+  pub ty: Box<Ty>,
+  pub expr: Box<Expression>,
+}
+
+#[derive(Debug)]
+pub struct Const {
+  pub idx: NodeIdx,
+
+  pub attrs: Vec<Attribute>,
+  pub vis: Visibility,
+  pub ident: Ident,
+  pub generics: Generics,
+  pub ty: Box<Ty>,
+  pub expr: Box<Expression>,
+
+  pub defaultness: Defaultness,
 }
 
 
+#[derive(Debug)]
+pub struct ForeignKorpe {
+  pub extern_span: Span,
+
+  pub safety: Safety,
+  pub abi: Option<Symbol>,
+  pub items: Vec<Item<ForeignItemKind>>,
+}
+#[derive(Debug)]
+pub enum Korpe {
+  Braced(Vec<Item>, Span),
+  File,
+}
 #[derive(Debug)]
 pub struct Path {
   pub segments: Vec<PathSegment>,
@@ -217,4 +377,41 @@ pub enum UseKind {
   Multiple(Vec<UseKind>),
 
   Everything,
+}
+
+#[derive(Debug)]
+pub struct Visibility {
+  pub kind: VisibilityKind,
+  pub span: Span,
+}
+#[derive(Debug)]
+pub enum VisibilityKind {
+  Public,
+  Private(Box<Path>),
+  Protected,
+}
+
+#[derive(Debug)]
+pub struct DelimArgs {
+  pub spacing: DelimSpacing,
+  pub span: DelimSpan,
+  pub tokens: TokenStream,
+}
+
+
+#[derive(Debug)]
+pub enum Safety {
+  Safe(Span),
+  Unsafe(Span),
+  Inherit,
+}
+#[derive(Debug)]
+pub enum Mutability {
+  Mut,
+  Nope,
+}
+#[derive(Debug)]
+pub enum Defaultness {
+  Default,
+  Overriden,
 }
